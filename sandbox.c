23d5904d6da5cbaae24478b5cb5f283db69dc47f
rafaelmessias <rmmartins@gmail.com>
2021-05-25 23:10:10 +0200

    Some colors for the sandbox

--- sandbox.c
+++ sandbox.c
@@ -12,16 +12,20 @@ GLuint Vbo, Vao;
 GLchar* vertexSrc = \
     "#version 150\n\
     in vec3 in_Position;\
+    in vec3 in_Color;\
+    out vec3 color;\
     void main(void) {\
+        color = in_Color;\
         gl_Position = vec4(in_Position, 1.0);\
     }";
 
 GLchar* fragSrc = \
     "#version 150\n\
     precision highp float;\
+    in vec3 color;\
     out vec4 fragColor;\
     void main(void) {\
-        fragColor = vec4(0.0, 1.0, 0.0, 1.0);\
+        fragColor = vec4(color, 1.0);\
     }";
 
 
@@ -52,24 +56,65 @@ void initSystem()
     glGenBuffers(1, &Vbo);
     glBindBuffer(GL_ARRAY_BUFFER, Vbo);    
 	glGenVertexArrays(1, &Vao);
-        glBindVertexArray(Vao);    
-        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0); 
+    glBindVertexArray(Vao);
+        int stride = 6 * sizeof(GL_FLOAT);
+        const void *colorOffset = (const void *)(3 * sizeof(GL_FLOAT));
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, stride, 0); 
         glEnableVertexAttribArray(0);
+        glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, stride, colorOffset); 
+        glEnableVertexAttribArray(1);
     glBindVertexArray(0);   
 
     GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
     glShaderSource(vertexShader, 1, (const GLchar**)&vertexSrc, 0);
     glCompileShader(vertexShader);
 
+    int IsCompiled_VS, maxLength;
+    char *vertexInfoLog;
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &IsCompiled_VS);
+    if(IsCompiled_VS == GL_FALSE)
+    {
+       glGetShaderiv(vertexShader, GL_INFO_LOG_LENGTH, &maxLength);
+
+       /* The maxLength includes the NULL character */
+       vertexInfoLog = (char *)malloc(maxLength);
+
+       glGetShaderInfoLog(vertexShader, maxLength, &maxLength, vertexInfoLog);
+
+       /* Handle the error in an appropriate way such as displaying a message or writing to a log file. */
+       printf("%s", vertexInfoLog);
+       /* In this simple program, we'll just leave */
+       free(vertexInfoLog);
+       exit(-1);
+    }
+
     GLuint fragShader = glCreateShader(GL_FRAGMENT_SHADER);
     glShaderSource(fragShader, 1, (const GLchar**)&fragSrc, 0);
     glCompileShader(fragShader);
+
+    glGetShaderiv(fragShader, GL_COMPILE_STATUS, &IsCompiled_VS);
+    if(IsCompiled_VS == GL_FALSE)
+    {
+       glGetShaderiv(fragShader, GL_INFO_LOG_LENGTH, &maxLength);
+
+       /* The maxLength includes the NULL character */
+       vertexInfoLog = (char *)malloc(maxLength);
+
+       glGetShaderInfoLog(fragShader, maxLength, &maxLength, vertexInfoLog);
+
+       /* Handle the error in an appropriate way such as displaying a message or writing to a log file. */
+       printf("%s", vertexInfoLog);
+       /* In this simple program, we'll just leave */
+       free(vertexInfoLog);
+       exit(-1);
+    }
 
     GLuint shaderProg = glCreateProgram();
     glAttachShader(shaderProg, vertexShader);
     glAttachShader(shaderProg, fragShader);
     // This connects VAO position 0 (set above) to the shader variable in_Position
     glBindAttribLocation(shaderProg, 0, "in_Position");
+    glBindAttribLocation(shaderProg, 1, "in_Color");
     glLinkProgram(shaderProg);
     glUseProgram(shaderProg);
 }
@@ -80,9 +125,9 @@ int main(int argc, char *argv[])
 
     float vertices[] =
     {
-        -0.5, -0.5, 0.0,
-         0.5, -0.5, 0.0,
-         0.0,  0.5, 0.0
+        -0.5, -0.5, 0.0,    1, 0, 0,
+         0.5, -0.5, 0.0,    0, 1, 0,
+         0.0,  0.5, 0.0,    0, 0, 1
     };
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
